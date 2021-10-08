31da60a9ae204f13d4d38d9a41bc149fe1a8627d
Elia Robyn Speer <elia@explosion.ai>
2021-10-08 12:03:26 -0400

    tweak the arguments to the spaCy reader

--- spacious_corpus/corpus.py
+++ spacious_corpus/corpus.py
@@ -11,17 +11,27 @@ from pathlib import Path
 
 
 @registry.readers("spacious_corpus.SpaciousCorpus.v1")
-def acquire_corpus(
-    corpus_subpath: str,
-    lang: str,
-    limit: Optional[int],
+def corpus_reader(
+    corpus_name: str,
     workdir: Union[Path, str],
+    limit: Optional[int] = None,
 ) -> Callable[[Language], Iterable[Doc]]:
-    
+    """
+    Run the Snakemake build to acquire a corpus if necessary, then iterate
+    over its documents.
+
+    The corpus will be looked up in the Snakemake working directory `workdir`,
+    or built in that directory if necessary.
+    """
     workdir = Path(workdir)
-    build_targets([corpus_subpath], str(workdir))
 
     def corpus_iterator(nlp: Language) -> Iterator[Doc]:
+        # TODO: align languages or allow language codes to be overridden
+        lang = nlp.lang
+
+        corpus_subpath = f"data/tokens/{corpus_name}/{lang}.zip"
+        build_targets([corpus_subpath], str(workdir))
+
         absolute_corpus_path = workdir / corpus_subpath
         corpus = DocZip.open(absolute_corpus_path, lang)
         if limit is not None:
